d99422dee12666ca289395b7c567c92e84b96eda
gcmaciel <gustavocmaciel2@gmail.com>
2020-12-25 10:20:10 -0300

    Change alert message

--- static/scripts.js
+++ static/scripts.js
@@ -2,19 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#copy-button').addEventListener('click', Copy);
   
 });
-  
-function Copy() {
 
-  /* Get the text field */
+function Copy() {
+  /*
+  Copy the password to clipboard.
+
+  https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+  */
+
+  // Get the text field
   var copyText = document.getElementById("text");
  
-  /* Select the text field */
+  // Select the text field
   copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  copyText.setSelectionRange(0, 99999); // For mobile devices
 
-  /* Copy the text inside the text field */
+  // Copy the text inside the text field
   document.execCommand("copy");
  
-  /* Alert the copied text */
-  alert("Password Copied: " + copyText.value); 
+  // Alert the copied text
+  alert("Copied: " + copyText.value); 
 }
