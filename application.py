c3a5870ba946d144b2de01c61f8f6c88292139d2
gcmaciel <gustavocmaciel2@gmail.com>
2020-12-25 11:56:59 -0300

    Add favicon

--- application.py
+++ application.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template, request
 
 from helpers import generate_password
 
+# Configure application
 app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/new-password", methods=["GET", "POST"])
 def new_password():
