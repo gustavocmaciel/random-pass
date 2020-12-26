96cf16f6ce50c41a081a86d36bef2bc0036f49f9
gcmaciel <gustavocmaciel2@gmail.com>
2020-12-26 09:53:25 -0300

    Remove unnecessary code

--- application.py
+++ application.py
@@ -5,9 +5,6 @@ from helpers import generate_password
 # Configure application
 app = Flask(__name__)
 
-# Ensure templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
 
 @app.route("/")
 def index():
